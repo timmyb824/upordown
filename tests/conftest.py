0149d0238f617d9890034dff6240657db4f91c9e
Tim Bryant <timothybryant3@gmail.com>
2022-09-25 22:30:06 -0400

    add refresh button and updated time to homepage

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,8 @@ from src.upordown.main import app
 @pytest.fixture
 def client():
     return app.test_client()
+
+@pytest.fixture
+def checkurls():
+    checkurls = ["https://www.google.com","https://www.msn.com","https://doesnotexist.co.uk","https://www.yahoo.com"]
+    return checkurls
